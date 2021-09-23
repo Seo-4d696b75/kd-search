98c392bd87d5db2e4e28f320c9e02ff8de8b0632
Seo-4d696b75 <s.kaoru509@gmail.com>
2021-09-23 13:39:56 +0900

    修正：テストデータ

--- src/sphere.test.ts
+++ src/sphere.test.ts
@@ -1,26 +1,29 @@
 import * as KdTree from "./index"
 
-var array = KdTree.randomPoints(10000, 130, 150, 30, 50)
+var array = KdTree.randomPoints(1000, -100, 0, -10, 70)
+array.push(
+  {x: 170, y:0}
+)
 var array1 = Array.from(array)
 var array2 = Array.from(array)
 
 function compareAndCheck(queries: Array<KdTree.Point2D>, points: Array<KdTree.Point2D>, tree: KdTree.SearchNode) {
   queries.forEach(query => {
-    points.sort((a, b) => KdTree.measure(a, query, true) - KdTree.measure(b, query, true))
+    points.sort((a, b) => KdTree.measure(a, query, KdTree.MeasureType.Geodesic) - KdTree.measure(b, query, KdTree.MeasureType.Geodesic))
     // various params: k, r
-    for (var i = 0; i < 100; i++) {
+    for (var i = 0; i < 10; i++) {
       // r = 0
       var k = Math.round(Math.random() * 20) + 1
-      var result = KdTree.searchNearest(tree, query, k, 0, true)
+      var result = KdTree.searchNearest(tree, query, k, 0, KdTree.MeasureType.Geodesic)
       if (!KdTree.pointArrayEquals(result, points.slice(0, result.length))) {
-        console.error(`not matched. k=${k} r=0`)
+        console.error(`not matched. k=${k} r=0 q=(${query.x},${query.y})`)
       }
       // r > 0
       k = Math.round(Math.random() * 5) + 1
       var r = Math.random() * 20
-      var result2 = KdTree.searchNearest(tree, query, k, 0, true)
+      var result2 = KdTree.searchNearest(tree, query, k, 0, KdTree.MeasureType.Geodesic)
       if (!KdTree.pointArrayEquals(result2, points.slice(0, result2.length))) {
-        console.error(`not matched. k=1 r=${r}`)
+        console.error(`not matched. k=1 r=${r} q=(${query.x},${query.y})`)
       }
     }
   })
@@ -32,7 +35,7 @@ describe("sphere", () => {
     tree = KdTree.buildTree(array2)
   })
   test("normal", () => {
-    compareAndCheck(KdTree.randomPoints(20, 130, 150, 30, 50), array1, tree)
+    compareAndCheck(KdTree.randomPoints(100, -180, 180, -90, 90), array1, tree)
   })
   test("singular", () => {
     var points: Array<KdTree.Point2D> = [
@@ -41,6 +44,14 @@ describe("sphere", () => {
       { x: 130, y: 0 },
       { x: 0, y: 90 },
       { x: 0, y: -90 },
+      { x: -180, y: 0 },
+      { x: -180, y: 30 },
+      { x: -180, y: 60 },
+      { x: -180, y: -30 },
+      { x: 180, y: 0 },
+      { x: 180, y: 30 },
+      { x: 180, y: 60 },
+      { x: 180, y: -30 },
     ]
     compareAndCheck(points, array1, tree)
   })
