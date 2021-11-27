96eb6b7b42f2d40646de4d85a486c4f5f3c14c13
Seo-4d696b75 <s.kaoru509@gmail.com>
2021-11-27 16:28:49 +0900

    [fix] 境界付近の探索

--- src/index.ts
+++ src/index.ts
@@ -427,6 +427,11 @@ if (process.argv[2]) {
   console.log("points size:", size * 100, "query size:", size)
   var array = randomPoints(size * 100, "points", -180, 180, -90, 90)
   var queries = randomPoints(size, "query", -180, 180, -90, 90)
+  queries.push(
+    { x: -180, y: 0 },
+    { x: 180, y: 10 },
+    { x: 175, y: -20 }
+  )
   var tree: SearchNode
   console.time("sort")
   queries.forEach(query => {
